0766652e8b22c61728e58caefa2f2c53f8008c93
Rook Soto <rooksoto@gmail.com>
2024-11-04 15:18:53 -0500

    Solves CodeWars: Reversed sequence

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 package com.rooksoto.hackerleetcoderankwars
 
 import com.rooksoto.hackerleetcoderankwars.problems.codewars.ReduceButGrow
+import com.rooksoto.hackerleetcoderankwars.problems.codewars.ReversedSequence
 
 fun main() {
     ReduceButGrow().evaluate()
+    ReversedSequence().evaluate()
 }
---
+++ src/main/kotlin/problems/codewars/ReversedSequence.kt
@@ -0,0 +1,30 @@
+package com.rooksoto.hackerleetcoderankwars.problems.codewars
+
+class ReversedSequence : CodeWarsProblem<Int, List<Int>> {
+    override val kyu: Int =
+        8
+    override val problemName: String =
+        "Reversed sequence"
+
+    override val problemDescription: String =
+        """
+            Build a function that returns an array of integers from n to 1 where n>0.
+        """.trimIndent()
+
+    override val testInput: Int =
+        5
+
+    override val testSolution: List<Int> =
+        listOf(5, 4, 3, 2, 1)
+
+    override fun solve(input: Int): List<Int> =
+        reverseSeq(input)
+
+    // region Solution
+
+    private fun reverseSeq(n: Int): List<Int> =
+        (n downTo 1).toList()
+
+    // endregion
+
+}
